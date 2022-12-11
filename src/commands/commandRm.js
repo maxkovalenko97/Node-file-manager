5bc72c68c21b48545d06564eeda63d059656d627
Max Kovalenko <tutmrmaxi@gmail.com>
2022-12-11 18:12:34 +0300

    feat: implement command 'rm'

---
+++ src/commands/commandRm.js
@@ -0,0 +1,11 @@
+import { showCurrentDirectory } from "../utils.js";
+import fs from "fs";
+import path from "path";
+
+export default async function goRm(pathToFile) {
+  fs.unlink(path.resolve(pathToFile), (err) => {
+    if (err) console.error('Operation failed');
+  });
+  showCurrentDirectory();
+}
+
--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import goAdd from "./commands/commandAdd.js";
 import goRn from "./commands/commandRn.js";
 import goCp from "./commands/commandCp.js";
 import goMv from "./commands/commandMv.js";
+import goRm from "./commands/commandRm.js";
 
 process.chdir(homedir()); // change directory
 
@@ -65,6 +66,9 @@ rl
         break;
       case 'mv':
         goMv(args[0], args[1]);
+        break;
+      case 'rm':
+        goRm(args[0]);
         break;
       default:
         console.log('Invalid input');
