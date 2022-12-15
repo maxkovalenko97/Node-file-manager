0638b547fcd1ca4a978d073cf823d94ab87e9ea6
Max Kovalenko <tutmrmaxi@gmail.com>
2022-12-15 20:53:33 +0300

    fix: add soriting to ls command

--- src/commands/commandLs.js
+++ src/commands/commandLs.js
@@ -9,9 +9,15 @@ export default async function goLs() {
       if (elem.isDirectory()) elem['type'] = 'directory'
       else elem['type'] = 'file';
     });
-    console.table(files);
+    const sortedArr = files.sort().sort((elem1, elem2) => {
+      if (elem1.type > elem2.type) return 1;
+      if (elem1.type == elem2.type) return 0;
+      if (elem1.type < elem2.type) return -1;
+    });
+    console.table(sortedArr);
     showCurrentDirectory();
   } catch (err) {
     console.error("FS operation failed");
+    showCurrentDirectory();
   }
 }
