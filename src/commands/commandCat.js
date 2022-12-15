0116630c29ada5952c0ed911adc1f0e1b01036b2
Max Kovalenko <tutmrmaxi@gmail.com>
2022-12-15 20:58:35 +0300

    fix: fix errors and end message

--- src/commands/commandCat.js
+++ src/commands/commandCat.js
@@ -6,10 +6,16 @@ export default async function goCat(pathFile) {
   try {
     const resolvedPath = resolve(pathFile);
     const readableStream = fs.createReadStream(resolvedPath, 'utf-8');
-    readableStream.on('error', () => console.error('Operation failed'));
+    readableStream.on('error', (error) => {
+      if (error) {
+        console.error('Operation failed');
+        showCurrentDirectory();
+      }
+    });
     readableStream.on('data', chunk => console.log(chunk));
     await readableStream.on('end', () => showCurrentDirectory());
   } catch (error) {
-    console.error('Operation failed')
+    console.error('Operation failed');
+    showCurrentDirectory();
   }
 }
